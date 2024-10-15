e41ce64180a478c24d63ad71125444777c48b44f
sorosmark <soros.mark@diak.szbi-pg.hu>
2024-10-15 10:25:39 +0200

    Send products date on anotherroute

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 products = []
 
-@app.route('/', methods = ["GET", "POST"])
+@app.route('/', methods = ["GET"])
 def index():
-    if request.method == "POST":
-        name = request.form.get("productname")
-        products.append(name)
-    return render_template("index.html", products_ = products)
+    return render_template("index.html")
+
+
+@app.route('/submit_products', methods = ["POST"])
+def submit_products():
+    name = request.form.get("productname")
+    products.append(name)
+    return redirect(url_for('index'))
+
+
+
 
 if __name__ == "__main__":
     app.run(debug = True)
