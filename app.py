b6f84dec160995dff65822cc8eabe31b825177b7
sorosmark <soros.mark@diak.szbi-pg.hu>
2024-10-15 09:48:54 +0200

    add templates folder

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+products = []
 
 @app.route('/', methods = ["GET", "POST"])
 def index():
-    name = None
     if request.method == "POST":
         name = request.form.get("productname")
-    return render_template("index.html", name = name)
+        products.append(name)
+    return render_template("index.html", products_ = products)
 
 if __name__ == "__main__":
     app.run(debug = True)
